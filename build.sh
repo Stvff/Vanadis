b5666006c9df36a8d826a82908915e018a5ade1b
0x4646 <65192177+StevenClifford@users.noreply.github.com>
2022-10-24 15:36:10 +0200

    New unified compilation script

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
-set -ex
+set -e
 
-gcc -lm -Wall -g3 -fsanitize=address vic.c -o vic
+#gcc -lm -Wall -g3 -fsanitize=address vic.c -o vic
 #gcc vic.c -lm -Wall -o vic
-#gcc vic.c -lm -Wall -Os -o vic
+if [[ $1 == fast ]]; then
+	echo "fast"
+	gcc vic.c -lm -Wall -Os -o vic
+elif [[ $1 == lib ]]; then
+	gcc libvic.c -lm -Wall -Os -c -fPIC
+	gcc libvic.o -shared -o libvic.so
+	rm libvic.o
+	#sudo cp libvic.so /lib64/
+	gcc vic.c -lm -lvic -D libraryincluded -Wall -Os -o vic
+else
+	gcc -lm -Wall -g3 -fsanitize=address vic.c -o vic
+fi
 
 cp Vanadis.yaml ~/.config/micro/syntax/
 ./vic test.vas
